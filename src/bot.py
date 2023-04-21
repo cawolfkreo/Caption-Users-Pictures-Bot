1b72a94a237787d10f88bbc078e688b2a39d1fa5
Cawolf <camilozambranovotto@hotmail.com>
2023-04-21 00:54:34 -0500

    Updated python-telegram-bot to v20 :godmode:

--- src/bot.py
+++ src/bot.py
@@ -1,14 +1,14 @@
 import logging
 from datetime import datetime
 from pathlib import Path
-from telegram import MessageEntity, ChatAction, Update, UserProfilePhotos
+from telegram import MessageEntity, Update, UserProfilePhotos
+from telegram.constants import ChatAction
 from telegram.ext import (
+    Application,
     CommandHandler,
-    Filters,
+    filters,
     MessageHandler,
     PicklePersistence,
-    Updater,
-    callbackcontext,
     ContextTypes)
 from textManager import (
     getMentions,
@@ -30,31 +30,31 @@ STARTUP_TIME = datetime.now().timestamp()
 Constant for the current bot status
 '''
 
-def start(update: Update, context: callbackcontext.CallbackContext):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     '''
     This is the function called by the bot
     when the "/start" command is executed.
     '''
-    context.bot.send_message(
+    await context.bot.send_message(
         chat_id=update.effective_chat.id, 
         text="Hi! I am a bot made to add messages to profiles pictures of "+
         "people on group chats! just add me to a group chat and wait for the "+
         "magic to happen!")
 
-def about(update: Update, context: callbackcontext.CallbackContext):
+async def about(update: Update, context: ContextTypes.DEFAULT_TYPE):
     '''
     This is the function called by the bot
     when the "/about" command is executed.
     '''
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, 
+    await context.bot.send_message(
+        chat_id= update.effective_chat.id, 
         text=f"Hello! I am a bot 🤖 made by **@Cawolf** to randomly "+
         "caption people's profile pictures. You can find my source "+
         "code [on this github repository.]"+
         "(https://github.com/cawolfkreo/Caption-Users-Picures-Bot)",
         parse_mode="Markdown")
 
-def text(update: Update, context: callbackcontext.CallbackContext):
+async def text(update: Update, context: ContextTypes.DEFAULT_TYPE):
     '''
     This function is called by the bot
     when it sees a text message.
@@ -79,19 +79,19 @@ def text(update: Update, context: callbackcontext.CallbackContext):
     if not(telegramUserId and message and validMessageLength(message.text, mention)):
         return
     
-    context.bot.sendChatAction(
+    await context.bot.sendChatAction(
         chat_id = update.effective_chat.id,
         action = ChatAction.UPLOAD_PHOTO)
 
-    userProfilePic = context.bot.getUserProfilePhotos(telegramUserId, limit = 1)
-    resultImage = processImage(userProfilePic, message.text, mention)
+    userProfilePic = await context.bot.getUserProfilePhotos(telegramUserId, limit = 1)
+    resultImage = await processImage(userProfilePic, message.text, mention)
     
     if(resultImage):
-        update.message.reply_photo(photo=resultImage,)
+        await update.message.reply_photo(photo=resultImage,)
     else:
         #if the user has no profile picture the bot will
         #default to this message as a reply.
-        context.bot.sendMessage(
+        await context.bot.sendMessage(
             chat_id = update.effective_chat.id, 
             text = ("Imagine this is the profile " +
                     f"picture of {mention} with the text " +
@@ -101,7 +101,7 @@ def text(update: Update, context: callbackcontext.CallbackContext):
                     "profile picture 😅"),
             reply_to_message_id = message.message_id)
         
-def evilMeme(update: Update, context: callbackcontext.CallbackContext):
+async def evilMeme(update: Update, context: ContextTypes.DEFAULT_TYPE):
     '''
     This is the function called by the bot
     when the bot sees the /evil command.
@@ -109,7 +109,7 @@ def evilMeme(update: Update, context: callbackcontext.CallbackContext):
     chat = update.effective_chat    
     if not (chat and isMessageFromAGroup(chat.type)):
         # We are not on a group
-        update.message.reply_text("Sorry you can only " +
+        await update.message.reply_text("Sorry you can only " +
                             "use this command in groups 😅")
         return
 
@@ -119,7 +119,7 @@ def evilMeme(update: Update, context: callbackcontext.CallbackContext):
     entities = update.message.parse_entities([MessageEntity.MENTION])
     if not entities:
         # the user didn't mentioned another user by their alias
-        update.message.reply_text("Sorry to use this command " +
+        await update.message.reply_text("Sorry to use this command " +
                             "you need to provide a username alias " +
                             "after the /evil 😅\nExample: " +
                             "/evil @someUserAlias name any bottom text here")
@@ -129,21 +129,21 @@ def evilMeme(update: Update, context: callbackcontext.CallbackContext):
     del context.args[0]
     if mention == context.bot.name:
         #sorry but I cannot use this on myself 
-        update.message.reply_text("sorry but I cannot use this on myself " +
+        await update.message.reply_text("sorry but I cannot use this on myself " +
                             "Try somebody else, I appreciate the gesture btw 🤖👍")
         return
 
     telegramUserId = getUserIdFromBotData(mention, context.bot_data)
     if not telegramUserId:
         # We couldn't find the userId
-        update.message.reply_text("Sorry for some reason I am not able " +
+        await update.message.reply_text("Sorry for some reason I am not able " +
                             "to find that user. Telegram is not allowing " +
                             "me to look it up at the moment 😓")
         return
     
     if (len(context.args) == 0 or not context.args[0]):
         # the user didn't provide the name
-        update.message.reply_text("Sorry to use this command " +
+        await update.message.reply_text("Sorry to use this command " +
                             "you need to provide the name of the " +
                             "user after the alias 😢.\n" 
                             "Example:\n " +
@@ -156,26 +156,26 @@ def evilMeme(update: Update, context: callbackcontext.CallbackContext):
     textToDisplay = " ".join(context.args).strip()
     if not(message and textToDisplay and validMessageLength(textToDisplay, mention)):
         # the user didn't provide the text
-        update.message.reply_text("Sorry to use this command " +
+        await update.message.reply_text("Sorry to use this command " +
                             "you need to provide the text " +
                             "to display at the bottom after " +
                             "the image 😢.\nExample:\n" +
                             "/evil @someUserAlias name any bottom text here")
         return
     
-    context.bot.sendChatAction(
+    await context.bot.sendChatAction(
         chat_id = update.effective_chat.id,
         action = ChatAction.UPLOAD_PHOTO)
 
-    userProfilePic: UserProfilePhotos = context.bot.getUserProfilePhotos(telegramUserId, limit = 1)
-    resultImage = processImage(userProfilePic, textToDisplay, mention, True, name)
+    userProfilePic: UserProfilePhotos = await context.bot.getUserProfilePhotos(telegramUserId, limit = 1)
+    resultImage = await processImage(userProfilePic, textToDisplay, mention, True, name)
     
     if resultImage:
-        update.message.reply_photo(photo=resultImage)
+        await update.message.reply_photo(photo=resultImage)
     else:
         #if the user has no profile picture the bot will
         #default to this message as a reply.
-        context.bot.sendMessage(
+        await context.bot.sendMessage(
             chat_id = update.effective_chat.id, 
             text = ("Imagine this is the profile " +
                     f"picture of {mention} with the text " +
@@ -191,7 +191,7 @@ when the bot sees any message. The idea
 is that the bot will store the userIDs
 of every group message it sees.
 '''
-def everything(update, context):
+async def everything(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat = update.effective_chat
     if(chat and isMessageFromAGroup(chat.type)):
         messageUser = update.effective_user
@@ -209,34 +209,38 @@ def startBot():
     and it's execution starts.
     '''
     Path("data").mkdir(parents=True, exist_ok=True)
-    botPersistent = PicklePersistence(filename="data/sav.almcn")
-    updater = Updater(token=TELEGRAM_API, persistence=botPersistent, use_context=True)
-    dispatcher = updater.dispatcher
+    botPersistent = PicklePersistence(filepath="data/sav.almcn")
 
-    start_handler = CommandHandler('start', start, run_async=True)
-    about_handler = CommandHandler('about', about, run_async=True)
-    text_handler = MessageHandler(Filters.text & (~Filters.command), text, run_async=True)
-    evil_handler = CommandHandler("evil", evilMeme, run_async=True)
-    everything_handler = MessageHandler(Filters.all, everything, run_async=True)
+    application = (
+        Application.builder()
+        .token(TELEGRAM_API)
+        .persistence(botPersistent)
+        .build()
+    )
 
-    dispatcher.add_handler(start_handler)                   #The start handler is given to the bot
-    dispatcher.add_handler(about_handler)                   #The about handler is given to the bot
-    dispatcher.add_handler(text_handler)                    #The text handler is given to the bot
-    dispatcher.add_handler(evil_handler)                    #The evil meme handler is given to the bot
-    dispatcher.add_handler(everything_handler, group = 1)   #The default handler is given to the bot
+    start_handler = CommandHandler('start', start)
+    about_handler = CommandHandler('about', about)
+    text_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), text)
+    evil_handler = CommandHandler("evil", evilMeme)
+    everything_handler = MessageHandler(filters.ALL, everything)
 
-    dispatcher.add_error_handler(error_handler)
+    application.add_handler(start_handler)                   #The start handler is given to the bot
+    application.add_handler(about_handler)                   #The about handler is given to the bot
+    application.add_handler(text_handler)                    #The text handler is given to the bot
+    application.add_handler(evil_handler)                    #The evil meme handler is given to the bot
+    application.add_handler(everything_handler, group = 1)   #The default handler is given to the bot
 
+    application.add_error_handler(error_handler)
+
+    printTime(logger, "The bot is starting... :)")
     if(ISPRODUCTION):
         webhook = f"https://{URL}/{TELEGRAM_API}"
-        updater.start_webhook(listen="0.0.0.0",
+        application.run_webhook(listen="0.0.0.0",
                                 port=PORT,
                                 url_path=TELEGRAM_API,
                                 webhook_url=webhook)
     else:
-        updater.start_polling()                             #Starts the bot 
-    printTime(logger, "The bot has started! :)")
-    updater.idle()                                          #Makes sure the bot stops when the ctrl+c signal is sent
+        application.run_polling()                             #Starts the bot 
     printTime(logger, "The bot stopped :C")
 
 '''
